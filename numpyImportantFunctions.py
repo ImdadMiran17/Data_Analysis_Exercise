ffd61e7b39e849cc29cf05b3a17bafb3e4cac199
Imdad Miran <abdullahbinrashed1225@gmail.com>
2021-06-22 13:41:54 +0600

    Create numpyImportantFunctions.py

---
+++ numpyImportantFunctions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+print("An Empty Array of size 5")
+print(np.empty(5)) # Generate empty array of size 5
+
+print("An Array of only 1's of size 5")
+print(np.ones(5)) # Generate array containing only 1's of size 5
+
+print("An Array of only 0's of size 5")
+print(np.zeros(5)) # Generate array containing only 0's of size 5
+
+print("An Identity Matrix Array of size 5")
+print(np.eye(5)) # Generate identity matrix array of size 5
+
+print("Array populated with numbers from 2 to 50 with the difference of 3")
+print(np.arange(2, 50, 3)) # Generate array with consecutive numbers
