806bc894192a10c3fcb30f47e38b1a39a6b476de
Imdad Miran <abdullahbinrashed1225@gmail.com>
2021-06-22 14:11:41 +0600

    Update slicing2D.py

--- slicing2D.py
+++ slicing2D.py
@@ -13,3 +13,9 @@ print(arr2d[1:3, 0:2])
 
 print("\nThe new sliced column")
 print(arr2d[:, 1:2])
+
+
+#In the first example, the last two rows and the first two columns are selected.
+#The second example selects all the rows, and only the center column to get the values of the middle column
+
+
